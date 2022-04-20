1a95ca764532b105d7c807f589cc0ea9ff0e6eec
Nishaagaran <59594637+Nishaagaran@users.noreply.github.com>
2022-04-20 14:31:48 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+
+public class Calculator {
+
+	
+	public int add(int a,int b)
+	{
+		int c=a+b;
+		return c;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calculator calc=new Calculator();
+int result=calc.add(10, 10);
+System.out.println(result);
+	}
+
+}
